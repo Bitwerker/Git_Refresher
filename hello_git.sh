512aa8f7be9a47c4005c331050dd0d494d035d97
Markus Wildner <markus-72@gmx.de>
2024-11-25 10:05:45 +0100

    Git initialisieren für hello_git.sh

---
+++ hello_git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#########################################
+# Author : Markus Wildner              	#
+# Created :	25.11.24		#
+# Modified : 	25.11.24		#
+#					#
+# Description : 			#
+# just a little Git starter		#
+# 					#
+# Usage : 				#
+# ./hello_git.sh			#
+# 					#
+######################################### 											
+
+clear
+
+echo  -e "Hello Git\nIch bin eine zweite Zeile"
+
+exit 0
