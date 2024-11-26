dfe90bc9759f532e727e6f7736591589ecfe3617
Markus Wildner <markus-72@gmx.de>
2024-11-26 10:49:22 +0100

    Ls command zum Testen eingebaut

--- hello_git.sh
+++ hello_git.sh
@@ -15,10 +15,32 @@
 
 clear
 
+file_destination="$HOME/File_dump"
+
 #echo  -e "Hello Git\nIch bin eine zweite Zeile"
 echo  -e "\tNeue Zeile eingerückt"
 echo  -e "Eingefügt über Laptop\nund gepusht"
 echo  -e "Erste Zeile auskommentiert"
+
+if [ -e "$file_destination" ]
+then
+	echo "Ordner File_dump existiert bereits"
+else
+	mkdir "$file_destination"
+fi
+
+
+for item in $( ls  "$HOME" ) 
+do
+	if [ -d "$HOME/$item" ]
+	then
+		echo "Das Item: $item ist ein Ordner"
+	elif [ -f "$HOME/$item" ]
+	then
+		echo "Das Item: $item ist eine Datei und wird verschoben"
+		mv "$HOME/$item"  "$file_destination/$item"
+	fi
+done
 
 exit 0
 
